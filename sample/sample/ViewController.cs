2e642d6165a7ccd4830cd8148948d5d587965e1d
Marc Bruins <marc@marcbruins.nl>
2017-05-16 21:30:06 +0200

    cleanup

--- sample/sample/ViewController.cs
+++ sample/sample/ViewController.cs
@@ -4,68 +4,68 @@ using UIKit;
 
 namespace sample
 {
-	public partial class ViewController : UIViewController, IBWWalkthroughViewControllerDelegate
-	{
-		protected ViewController(IntPtr handle) : base(handle)
-		{
-			// Note: this .ctor should not contain any initialization logic.
-		}
+    public partial class ViewController : UIViewController, IBWWalkthroughViewControllerDelegate
+    {
+        protected ViewController(IntPtr handle) : base(handle)
+        {
+            // Note: this .ctor should not contain any initialization logic.
+        }
 
-		public override void ViewDidLoad()
-		{
-			base.ViewDidLoad();
-			// Perform any additional setup after loading the view, typically from a nib.
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+            // Perform any additional setup after loading the view, typically from a nib.
 
-			btnShow.TouchUpInside += (s, e) => { ShowWalkthrough(); };
-		}
+            btnShow.TouchUpInside += (s, e) => { ShowWalkthrough(); };
+        }
 
-		public void ShowWalkthrough()
-		{
-			// Get view controllers and build the walkthrough
-			var stb = UIStoryboard.FromName("Walkthrough",null);
+        public void ShowWalkthrough()
+        {
+            // Get view controllers and build the walkthrough
+            var stb = UIStoryboard.FromName("Walkthrough", null);
 
-			BWWalkthroughViewController walkthrough = stb.InstantiateViewController("walk") as BWWalkthroughViewController;
-			var page_zero = stb.InstantiateViewController("walk0");
-			var page_one = stb.InstantiateViewController("walk1");
-			var page_two = stb.InstantiateViewController("walk2");
-			var page_three = stb.InstantiateViewController("walk3");
+            BWWalkthroughViewController walkthrough = stb.InstantiateViewController("walk") as BWWalkthroughViewController;
+            var page_zero = stb.InstantiateViewController("walk0");
+            var page_one = stb.InstantiateViewController("walk1");
+            var page_two = stb.InstantiateViewController("walk2");
+            var page_three = stb.InstantiateViewController("walk3");
 
-			// Attach the pages to the master
-			walkthrough.walkDelegate = this;
+            // Attach the pages to the master
+            walkthrough.walkDelegate = this;
 
-			walkthrough.AddViewController(page_one);
-			walkthrough.AddViewController(page_two);
-			walkthrough.AddViewController(page_three);
-			walkthrough.AddViewController(page_zero);
+            walkthrough.AddViewController(page_one);
+            walkthrough.AddViewController(page_two);
+            walkthrough.AddViewController(page_three);
+            walkthrough.AddViewController(page_zero);
 
-			this.PresentViewController(walkthrough, true, null);
-		}
+            this.PresentViewController(walkthrough, true, null);
+        }
 
-		public override void DidReceiveMemoryWarning()
-		{
-			base.DidReceiveMemoryWarning();
-			// Release any cached data, images, etc that aren't in use.
-		}
+        public override void DidReceiveMemoryWarning()
+        {
+            base.DidReceiveMemoryWarning();
+            // Release any cached data, images, etc that aren't in use.
+        }
 
-		public void WalkthroughCloseButtonPressed()
-		{
-			this.DismissViewController(true, null);
-		}
+        public void WalkthroughCloseButtonPressed()
+        {
+            this.DismissViewController(true, null);
+        }
 
-		public void WalkthroughNextButtonPressed()
-		{
-	//		throw new NotImplementedException();
-		}
+        public void WalkthroughNextButtonPressed()
+        {
+            //		throw new NotImplementedException();
+        }
 
-		public void WalkthroughPrevButtonPressed()
-		{
-		//	throw new NotImplementedException();
-		}
+        public void WalkthroughPrevButtonPressed()
+        {
+            //	throw new NotImplementedException();
+        }
 
-		public void WalkthroughPageDidChange(int pageNumber)
-		{
-		//	throw new NotImplementedException();
-		}
-	}
+        public void WalkthroughPageDidChange(int pageNumber)
+        {
+            //	throw new NotImplementedException();
+        }
+    }
 }
 
